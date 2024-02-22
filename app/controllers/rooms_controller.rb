f521778d7c40531c371476e95b38feee7c844c3c
Essoufi-oussama <87605323+Essoufi-oussama@users.noreply.github.com>
2024-02-22 11:59:13 +0100

    fixed a problem with search query

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,9 +2,10 @@ class RoomsController < ApplicationController
   def index
     @rooms = Room.all
     if params[:query].present?
+      # removes all digits from the address and compares it to the query
       sql_subquery = <<~SQL
       hotels.name ILIKE :query
-      OR (hotels.address ILIKE :query AND NOT hotels.address ~* '\\m\\d+\\M')
+      OR (regexp_replace(hotels.address, '\\d', '', 'g') ILIKE :query)
       OR rooms.price_per_night <= :query_integer
       SQL
       @rooms = Room.joins(:hotel).where(sql_subquery, query: "%#{params[:query]}%", query_integer: params[:query].to_i)
